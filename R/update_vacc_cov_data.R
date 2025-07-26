433376c8a0ba8d65fb36b0c96a5a67b9b25f063a
Nishant Kishore <ynm2@cdc.gov>
2025-07-26 11:31:09 -0400

    finalize the country extract

--- R/update_vacc_cov_data.R
+++ R/update_vacc_cov_data.R
@@ -34,6 +34,12 @@ update_vacc_cov_data <- function(tif_folder, edav, output_path){
     )
   }
 
+  if (!requireNamespace("exactextractr", quietly = TRUE)) {
+    stop('Package "terra" must be installed to use this function.',
+         .call = FALSE
+    )
+  }
+
   cli::cli_process_start("Downloading SIR spatial data",
                          "SIR spatial data downloaded")
   ctry <- load_clean_ctry_sp()
@@ -43,7 +49,54 @@ update_vacc_cov_data <- function(tif_folder, edav, output_path){
 
   tif_files <- list.files(tif_folder, pattern = "\\.tif$", full.names = TRUE)
 
-  my_brick2 <- terra::rast(tif_files)
+  raster_brick <- terra::rast(tif_files)
 
-  x <- exactextractr::exact_extract(my_brick2, ctry, fun = "mean")
+  #extracting the mean coverage by spatial area
+  ctry_extract <- exactextractr::exact_extract(raster_brick, ctry, fun = "mean")
+
+  #bind the columns of the spatial data and extracted output together
+  ctry_cov <- dplyr::bind_cols(
+    ctry |>
+      tibble::as_tibble() |>
+      dplyr::select(ADM0_NAME, GUID, yr.st, yr.end),
+    ctry_extract
+  ) |>
+    #turn into wide format to more easily modify col names
+    #the values here can change depending on how many years of data
+    #are available and vaccines included so they are dynamically
+    #chosen
+    tidyr::pivot_longer(names(x)[1]:names(x)[length(x)]) |>
+    #dropping all extracts without values
+    dplyr::filter(!is.na(value)) |>
+    #removing '.mean' from the name
+    dplyr::mutate(name = str_replace(name, "mean.", "")) |>
+    #the rest of the name is separated by "_", here we
+    #split them into 7 columns, this many need to be changed
+    #if the naming convention changes
+    tidyr::separate(name,
+                    c("vacc", "cov", "type", "method",
+                      "start", "end", "index")) |>
+    #select variables of interest
+    dplyr::select(ADM0_NAME, GUID, yr.st, yr.end, vacc,
+                  start, end, index, value) |>
+    #each name provides the range of years for which it is valid
+    #and the index that it refers to in that range so 2000_2023_1
+    #means that the range is from 2000 to 2023 and it's the first
+    #item in that range, so 2000
+    #we fist convert them all into integers
+    dplyr::mutate(
+      start = as.integer(start),
+      end = as.integer(end),
+      index = as.integer(index)
+    ) |>
+    #we then extract the specific year value
+    dplyr::rowwise() |>
+    dplyr::mutate(
+      year = (start:end)[index]
+    ) |>
+    dplyr::ungroup() |>
+    #we drop all inappropriate years
+    dplyr::filter(dplyr::between(year, yr.st, yr.end)) |>
+    dplyr::select(ADM0_NAME, GUID, year, vacc, value)
+
 }
