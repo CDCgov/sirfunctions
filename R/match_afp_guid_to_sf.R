78d79ae7f1bdb10da0df3ba9d6f90ab5fb7eeb13
Mervin Keith Cuadera <40894971+mcuadera@users.noreply.github.com>
2026-03-02 13:26:18 -0500

    anchor fix to shapefile rather than pop

--- R/match_afp_guid_to_sf.R
+++ R/match_afp_guid_to_sf.R
@@ -7,8 +7,8 @@
 #' AFP linelist.
 #'
 #' @param afp_data `tibble` AFP linelist.
-#' @param prov_pop `tibble` Province population file.
-#' @param dist_pop `tibble` District population file.
+#' @param prov_sf_long `tibble` Province shape file in long format (shapes for each year).
+#' @param dist_sf_long `tibble` District shape file in long format (shapes for each year).
 #'
 #' @returns `tibble` AFP linelist with corrected GUIDs
 #' @export
@@ -18,7 +18,7 @@
 #' raw_data <- get_all_polio_data()
 #' afp_fixed <- match_afp_guid_to_sf(raw_data$afp, raw_data$prov.pop, raw_data$dist.pop)
 #' }
-match_afp_guid_to_sf <- function(afp_data, prov_pop, dist_pop) {
+match_afp_guid_to_sf <- function(afp_data, prov_sf_long, dist_sf_long) {
 
   nrow_original <- afp_data |> nrow()
 
@@ -38,59 +38,63 @@ match_afp_guid_to_sf <- function(afp_data, prov_pop, dist_pop) {
                                  place.admin.2 = "dist",
                                  dateonset = "date",
                                  yronset = "year")
-  prov_pop <- dplyr::rename_with(prov_pop, recode,
-                                 ADM0_NAME = "ctry",
-                                 ADM1_NAME = "prov",
-                                 ADM0_GUID = "adm0guid")
 
-  dist_pop <- dplyr::rename_with(dist_pop, recode,
-                                 ADM0_NAME = "ctry",
-                                 ADM1_NAME = "prov",
-                                 ADM2_NAME = "dist",
-                                 ADM0_GUID = "adm0guid")
+  prov_lookup_sf <- prov_sf_long |>
+    dplyr::tibble() |>
+    dplyr::select(year = active.year.01,
+                  ctry = ADM0_NAME,
+                  prov = ADM1_NAME,
+                  adm0guid = ADM0_GUID,
+                  adm1guid_sf = GUID)
 
-  prov_lookup <- prov_pop |>
-    dplyr::select(year, ctry, prov, adm0guid, adm1guid_pop = adm1guid)
   # Forward to current year if missing
-  if (!lubridate::year(Sys.Date()) %in% unique(prov_lookup$year)) {
-    curr_year <- prov_lookup |>
+  if (!lubridate::year(Sys.Date()) %in% unique(prov_lookup_sf$year)) {
+    curr_year_sf <- prov_lookup_sf |>
       filter(year == max(year, na.rm = T)) |>
       mutate(year = lubridate::year(Sys.Date()))
-
-    prov_lookup <- dplyr::bind_rows(prov_lookup, curr_year)
+    prov_lookup_sf <- dplyr::bind_rows(prov_lookup_sf, curr_year_sf)
   }
-  dist_lookup <- dist_pop |>
-    dplyr::select(year, ctry, prov, dist, adm0guid, adm1guid, adm2guid_pop = adm2guid)
 
-  if (!lubridate::year(Sys.Date()) %in% unique(dist_lookup$year)) {
-    curr_year <- dist_lookup |>
+  dist_lookup_sf <- dist_sf_long |>
+    dplyr::tibble() |>
+    dplyr::select(year = active.year.01,
+                  ctry = ADM0_NAME,
+                  prov = ADM1_NAME,
+                  dist = ADM2_NAME,
+                  adm0guid = ADM0_GUID,
+                  adm1guid = ADM1_GUID,
+                  adm2guid_sf = GUID)
+
+  if (!lubridate::year(Sys.Date()) %in% unique(dist_lookup_sf$year)) {
+    curr_year_sf <- dist_lookup_sf |>
       filter(year == max(year, na.rm = T)) |>
       mutate(year = lubridate::year(Sys.Date()))
 
-    dist_lookup <- dplyr::bind_rows(dist_lookup, curr_year)
+    dist_lookup_sf <- dplyr::bind_rows(dist_lookup_sf, curr_year_sf)
   }
 
   # Manually fix NAROK WEST and BURURI
-  dist_lookup <- dist_lookup |>
-    filter(!(dist == "BURURI" & adm2guid_pop == "{4A72EFCA-4E8C-4439-98B9-84BA7ECBDB7F}" & year >= 2023)) |>
-    filter(!(dist == "BURURI" & adm2guid_pop == "{44AB3985-6B4A-4A97-A3B7-82E03420F9C3}" & year == 2020)) |>
-    filter(!(dist == "NAROK WEST" & adm2guid_pop == "{723D75B4-76BD-4C66-9786-474AD09C72A7}" & year >= 2024))
+  dist_lookup_sf <- dist_lookup_sf |>
+    filter(!(dist == "BURURI" & adm2guid_sf == "{4A72EFCA-4E8C-4439-98B9-84BA7ECBDB7F}" & year >= 2023)) |>
+    filter(!(dist == "BURURI" & adm2guid_sf == "{44AB3985-6B4A-4A97-A3B7-82E03420F9C3}" & year == 2020)) |>
+    filter(!(dist == "NAROK WEST" & adm2guid_sf == "{723D75B4-76BD-4C66-9786-474AD09C72A7}" & year >= 2024))
 
-  # Overwrite to reflect what's on the pop files
+  # Overwrite to reflect what's on the shapefiles
   afp_data_fixed <- afp_data |>
-    dplyr::left_join(prov_lookup) |>
-    dplyr::mutate(fixed_prov = adm1guid != adm1guid_pop,
-                  adm1guid = adm1guid_pop)
+    dplyr::left_join(prov_lookup_sf) |>
+    dplyr::mutate(fixed_prov_sf = adm1guid != adm1guid_sf,
+                  adm1guid = adm1guid_sf) |>
+    dplyr::select(-adm1guid_sf)
 
   afp_data_fixed <- afp_data_fixed |>
-    dplyr::left_join(dist_lookup) |>
-    dplyr::mutate(fixed_dist = adm2guid != adm2guid_pop,
-                  adm2guid = adm2guid_pop) |>
-    dplyr::select(-adm2guid_pop)
+    dplyr::left_join(dist_lookup_sf) |>
+    dplyr::mutate(fixed_dist_sf = adm2guid != adm2guid_sf,
+                  adm2guid = adm2guid_sf) |>
+    dplyr::select(-adm2guid_sf)
 
   # Info on fixes
-  cli::cli_alert_info(paste0(sum(afp_data_fixed$fixed_prov, na.rm = T), " provinces fixed"))
-  cli::cli_alert_info(paste0(sum(afp_data_fixed$fixed_dist, na.rm = T), " districts fixed"))
+  cli::cli_alert_info(paste0(sum(afp_data_fixed$fixed_prov_sf, na.rm = T), " provinces fixed"))
+  cli::cli_alert_info(paste0(sum(afp_data_fixed$fixed_dist_sf, na.rm = T), " districts fixed"))
 
   # Rename column names to original if cleaning global polio data
   if (global_afp) {
