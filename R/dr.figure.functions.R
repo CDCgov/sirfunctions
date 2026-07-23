f5df32f5d46d4754c8f6eabe9663f840fda306e5
Michelle Sloan <jtq4@cdc.gov>
2026-07-23 12:57:10 -0400

    Update dr.figure.functions.R
    update figure description

--- R/dr.figure.functions.R
+++ R/dr.figure.functions.R
@@ -772,8 +772,7 @@ generate_es_timely <- function(es.data,
 #' Immunization rates per year
 #'
 #' Generates a stacked percent bar plot displaying immunization rates per year for the country.
-#' Note that this function only graphs immunization rates for children aged 6-59 months that have
-#' the classification of NPAFP.
+#' Note that this function only graphs immunization rates for children aged 6-59 months.
 #'
 #' @param ctry.data `list` A large list containing polio data of country.
 #' This is the output of [extract_country_data()] or [init_dr()]. Note that `ctry_data` needs to be cleaned
